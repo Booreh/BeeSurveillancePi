e925429d5738203f06271e2f7bf3ae9b57962eba
Martin Bore <martin.boreh@outlook.com>
2024-03-11 21:31:20 +0100

    Create image.py
    -Added image class for working with images.

---
+++ src/domain/image.py
@@ -0,0 +1,16 @@
+
+class Image:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def display(self):
+        #code to display the image
+        pass
+
+    def resize(self, width, height):
+        #code to resize the image
+        pass
+
+    def rotate(self, angle):
+        #code to rotate the image
+        pass

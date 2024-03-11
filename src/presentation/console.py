319407f7a7598f36c36e3c35cec1b9d3a86a2d96
Martin Bore <martin.boreh@outlook.com>
2024-03-11 21:48:57 +0100

    Create console.py
    -Added console for data that will be shown to user.

---
+++ src/presentation/console.py
@@ -0,0 +1,38 @@
+
+class Console:
+    def __init__(self, name):
+        self.name = name
+
+
+    def printWelcomeMessage(self):
+        print(f"RPi Bee Surveilance System - {self.name}")
+
+    def inputStartProcess(self):
+        return input("Do you want to start the process? (y/n): ")
+
+    def loggingCameraStatus(self):
+        print("Camera has status: x")
+
+    def statusRunning(self):
+        print("Running...")
+
+
+    def statusExiting(self):
+        print("Exiting...")
+
+    def runningSetupProcedures(self):
+        print("Running setup procedures...")
+
+    def statusImageSaved(self, image_path):
+        print(f"Image captured and successfully saved to {image_path}.")
+
+    def errorSetupFailed(self):
+        print("Setup procedure failed, please check logs for details.")
+
+    
+
+#print("Running...")
+#print("Exiting...")
+#print("Image captured and uploaded successfully.")
+
+# Logging?
